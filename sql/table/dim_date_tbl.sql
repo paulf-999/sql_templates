326c05f97cda43afb7c6480894dd6a52d5c93f5e
Paul Fry <paul-fry@hotmail.com>
2021-08-13 15:27:24 +1000

    resolved linting issues

--- sql/table/dim_date_tbl.sql
+++ sql/table/dim_date_tbl.sql
@@ -1,16 +1,18 @@
 CREATE TABLE IF NOT EXISTS DIM_DATE AS (
-    WITH date_seq AS (
-        SELECT DATEADD(DAY, SEQ4(), '2017-01-01') AS my_date
-        FROM TABLE (GENERATOR(ROWCOUNT => 4000))
+    WITH DATE_SEQ AS (
+        SELECT DATEADD(DAY, SEQ4(), '2017-01-01') AS MY_DATE
+        FROM TABLE(GENERATOR(ROWCOUNT => 4000))
     )
-    SELECT  TO_DATE(my_date)        AS forecast_date
-            , TO_TIMESTAMP(my_date) AS datetime
-            , YEAR(my_date)         AS year
-            , MONTH(my_date)        AS month
-            , MONTHNAME(my_date)    AS monthname
-            , DAY(my_date)          AS day
-            , DAYOFWEEK(my_date)    AS dayofweek
-            , WEEKOFYEAR(my_date)   AS weekofyear
-            , DAYOFYEAR(my_date)    AS dayofyear
-    FROM date_seq
+
+    SELECT
+        TO_DATE(MY_DATE) AS FORECAST_DATE
+        , TO_TIMESTAMP(MY_DATE) AS DATETIME
+        , YEAR(MY_DATE) AS YEAR
+        , MONTH(MY_DATE) AS MONTH
+        , MONTHNAME(MY_DATE) AS MONTHNAME
+        , DAY(MY_DATE) AS DAY
+        , DAYOFWEEK(MY_DATE) AS DAYOFWEEK
+        , WEEKOFYEAR(MY_DATE) AS WEEKOFYEAR
+        , DAYOFYEAR(MY_DATE) AS DAYOFYEAR
+    FROM DATE_SEQ
 );
