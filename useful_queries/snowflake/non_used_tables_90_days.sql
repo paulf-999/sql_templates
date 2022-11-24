accd0375ca565c4c23efc4b5df8f60797c170114
Paul Fry <paul-fry@hotmail.com>
2022-11-24 12:11:59 +1100

    tidying up query 'not_used_tables_90_days.sql'

--- useful_queries/snowflake/non_used_tables_90_days.sql
+++ useful_queries/snowflake/non_used_tables_90_days.sql
@@ -1,7 +1,7 @@
 -- query to show tables not being queried in last 90 days (change line 10 to define a different time period)
 WITH tables_recent AS (
 
-    SELECT f1.value:"objectName"::string AS tn
+    SELECT f1.value:"objectName"::string AS table_name_recent
     FROM snowflake.account_usage.access_history AS access_history
     , LATERAL FLATTEN(base_objects_accessed) AS f1
     WHERE
@@ -14,14 +14,22 @@ WITH tables_recent AS (
 , tables_all AS (
 
     SELECT
-        table_id::integer AS tid
-        , table_catalog || '.' || table_schema || '.' || table_name AS tn1
+        table_id::integer AS table_id
+        , table_catalog AS db
+        , table_schema AS table_schema
+        , table_name
+        , table_catalog || '.' || table_schema || '.' || table_name AS tbl_namespace
     FROM snowflake.account_usage.tables
     WHERE deleted IS NULL
 )
 
-SELECT *
+SELECT
+    table_id
+    , tbl_namespace
+    , db
+    , table_schema
+    , table_name
 FROM tables_all
-WHERE tn1 NOT IN (
-    SELECT tn FROM tables_recent
+WHERE tbl_namespace NOT IN (
+    SELECT table_name_recent FROM tables_recent
 );
